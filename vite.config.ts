6cfcb92a55874f180b0168cadfe8060adf6ba6e1
Connor Kress <ckress@ufl.edu>
2026-03-20 23:48:30 -0400

    update runelink/sdk dependency to use git submodules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +9,10 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@runelink/sdk": path.resolve(__dirname, "../runelink-sdk-js/src/index.ts"),
+      "@runelink/sdk": path.resolve(
+        __dirname,
+        "./vendor/runelink-sdk-js/src/index.ts"
+      ),
     },
   },
-})
+});
